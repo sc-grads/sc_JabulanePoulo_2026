39da7bc912d26e5203f3e7837ab945d81f9fcb71
Jabulane Poulo <jabulane.poulo@sambeconsulting.com>
2026-04-02 09:51:57 +0200

    Mad Libs

--- Python for beginners/main.py
+++ Python for beginners/main.py
@@ -17,3 +17,32 @@ print(my_fist)
 my_second = my_list[1]
 print(my_second)
 """
+"""
+users = {"bob": 1, "luigi": 2}
+user_id = users["bob"]
+users["mario"] = 3  # Adds a new key-value pair
+users["luigi"] = 4  # Modifies the existing key-value pair
+del users["bob"]    # Removes the key "bob" and its associated value
+
+weather_data = {
+    "time": "12:00",
+    "weather": {
+        "morning": "rain",
+        "evening": "more rain"
+    }
+}
+"""
+
+name: str = input('Enter a name: ')
+noun_a: str = input('Enter a noun: ')
+verb_a: str = input('Enter a verb: ')
+number_a: str = input('Enter a number: ')
+number_b: str = input('Enter another number: ')
+
+
+story: str = f"""
+----------------------------------------------------------------------
+This is a story about {name}, a strong (and beautiful) {noun_a} 
+loved to {verb_a}.
+"""
+print(story)
