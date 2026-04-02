a3ca4573f6f8589ba1966a86866d7fdf125b5bb3
Jabulane Poulo <jabulane.poulo@sambeconsulting.com>
2026-04-02 08:47:26 +0200

    Add files

---
+++ Python for beginners/main.py
@@ -0,0 +1,19 @@
+
+"""print("Welcome to your simple adder")
+print(30* "-")
+
+a: str = input("Enter a value for a :")
+b: str = input("Enter another value for b :")
+
+print(30* "-")
+
+print("The result is : ", int(a) + int(b))
+"""
+
+"""my_list = ["Alice", 30, True, [1, 2, 3]]
+
+my_fist = my_list[0]
+print(my_fist)
+my_second = my_list[1]
+print(my_second)
+"""
