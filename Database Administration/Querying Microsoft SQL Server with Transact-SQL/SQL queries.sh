5876c9f84fe68a12992ab8cf95ed450f3f075c71
Jabulane Poulo <jabulane.poulo@sambeconsulting.com>
2026-06-01 08:09:05 +0200

    Add examples for UNION and UNION ALL operations with various data type conversions

--- Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
+++ Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
@@ -575,4 +575,27 @@ set TotalAmount = +2.77
 where TotalAmount = -2.77 and EmployeeNumber = 132
 rollback tran
 
+--UNION and UNION all
+select * from inserted
+union 
+select * from deleted
+
+select convert(char(5),'hi') as Greeting
+union all
+select convert(char(11),'hello there') as GreetingNow
+union all
+select convert(char(11),'bonjour')
+union all
+select convert(char(11),'hi')
+
+
+select convert(tinyint, 45) as Mycolumn
+union
+select convert(bigint, 456)
+
+select 4
+union
+select 'hi there'
+
+
 
