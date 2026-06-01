72722749fe8a9cc69e3dfb5d3f6743e4ced2b4d3
Jabulane Poulo <jabulane.poulo@sambeconsulting.com>
2026-06-01 08:11:59 +0200

    Add examples for EXCEPT and INTERSECT operations, including CASE statements and data manipulation

--- Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
+++ Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
@@ -598,4 +598,45 @@ union
 select 'hi there'
 
 
+--Except and Intersect
+select *, Row_Number() over(order by (select null)) % 3 as ShouldIDelete
+--into tblTransactionNew
+from tblTransaction
 
+delete from tblTransactionNew
+where ShouldIDelete = 1
+
+update tblTransactionNew
+set DateOfTransaction = dateadd(day,1,DateOfTransaction)
+Where ShouldIDelete = 2
+
+alter table tblTransactionNew
+drop column ShouldIDelete
+
+select * from tblTransaction -- 2486 rows
+intersect--except--union--union all
+select * from tblTransactionNew -- 1657 rows, 829 changed rows, 828 unchanged
+order by EmployeeNumber
+
+
+
+--CASE
+declare @myOption as varchar(10) = 'Option C'
+
+select case when @myOption = 'Option A' then 'First option'
+            when @myOption = 'Option B' then 'Second option'
+			--else 'No Option' 
+			END as MyOptions
+go
+declare @myOption as varchar(10) = 'Option A'
+
+select case @myOption when 'Option A' then 'First option'
+                   when 'Option B' then 'Second option' 
+				   else 'No Option' END as MyOptions
+go
+
+
+	  case when left(EmployeeGovernmentID,1)='A' then 'Letter A'
+	       when EmployeeNumber<200 then 'Less than 200'
+		   else 'Neither letter' END + '.' as myCol
+  FROM tblEmployee
