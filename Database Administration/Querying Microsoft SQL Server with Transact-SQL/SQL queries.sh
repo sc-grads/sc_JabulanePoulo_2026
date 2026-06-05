612c155577216138bfe3e1f0b7703a0e77a10b8d
Jabulane Poulo <jabulane.poulo@sambeconsulting.com>
2026-06-05 13:45:35 +0200

    clean up

--- Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
+++ Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
@@ -2179,7 +2179,5 @@ select E.EmployeeNumber, sum(T.Amount) as TotalAmount
 from dbo.tblEmployee as E
 left join dbo.tblTransaction as T on E.EmployeeNumber = T.EmployeeNumber
 group by E.EmployeeNumber
-ll
-/ddd
-print 'Hello World'
-gh
+
+/ddddfhgdgfkk
