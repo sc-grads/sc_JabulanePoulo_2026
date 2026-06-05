55626995425842a648d77f57c2c6083f9920a173
Jabulane Poulo <jabulane.poulo@sambeconsulting.com>
2026-06-05 12:36:46 +0200

    Fix syntax error and add print statement in SQL queries script

--- Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
+++ Database Administration/Querying Microsoft SQL Server with Transact-SQL/SQL queries.sh
@@ -2179,6 +2179,6 @@ select E.EmployeeNumber, sum(T.Amount) as TotalAmount
 from dbo.tblEmployee as E
 left join dbo.tblTransaction as T on E.EmployeeNumber = T.EmployeeNumber
 group by E.EmployeeNumber
-
+ll
 /ddd
 print 'Hello World'
