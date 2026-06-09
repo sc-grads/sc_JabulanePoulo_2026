0e83fb123f7792a670b17a32aedde7cbced6ac59
Jabulane Poulo <jabulane.poulo@sambeconsulting.com>
2026-06-09 13:47:05 +0200

    Refactor deployment workflows: remove old deploy.yml and add reusable-deploy.yml

--- Database/deploy.sql
+++ Database/deploy.sql
@@ -1,4 +1,4 @@
--- Create database
+
 IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Cloud-tunnel-JP')
 BEGIN
     EXECUTE('CREATE DATABASE [Cloud-tunnel-JP]');
@@ -8,7 +8,7 @@ GO
 USE [Cloud-tunnel-JP];
 GO
 
--- Create simple table
+
 CREATE TABLE People (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     FirstName NVARCHAR(50),
